8aa8b6879c5ed8b08d5ae0170ce21d625e1a493e
Alvaro Narváez <anarvaezsa@ransa.net>
2024-06-13 07:53:02 -0500

    fix run

--- src/usuarios/usuarios.module.ts
+++ src/usuarios/usuarios.module.ts
@@ -3,9 +3,16 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsuariosService } from './usuarios.service';
 import { UsuariosController } from './usuarios.controller';
 import { Usuarios } from '../common/entities/usuarios.entity';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Usuarios])],
+  imports: [
+    TypeOrmModule.forFeature([Usuarios]),
+    JwtModule.register({
+      secret: 'your_jwt_secret',
+      signOptions: { expiresIn: '60m' },
+    })
+  ],
   providers: [UsuariosService],
   controllers: [UsuariosController],
 })
